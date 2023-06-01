910f34685daacbc8664f47ee863d8cf46df4e0ee
PatrickSampa <sampa.mendonca@gmail.com>
2023-06-01 01:24:06 -0300

    inserindo o  sweetAlert

--- src/views/scripts/cadastrarCliente.js
+++ src/views/scripts/cadastrarCliente.js
@@ -16,8 +16,26 @@ let cadastrar = document.getElementById('cadastrar');
 
 cadastrar.addEventListener("submit", async (e) => {
     e.preventDefault();
-    let a = document.getElementById('consultar');
-    a.click();
+    /* let a = document.getElementById('consultar');
+    a.click(); */
+    nome.value  = "";
+    dataNasc.value  = "";
+    email.value  = "";
+    telefone.value  = "";
+    cpf.value  = "";
+    rg.value  = "";
+    cidade.value  = "";
+    bairro.value  = "";
+    rua.value  = "";
+    casa.value  = "";
+    refen.value  = "";
+    obs.value  = "";
+    cadastrar.value = "";
+    Swal.fire(
+        'Sucesso!!',
+        'Cliente Cadastrado',
+        'success'
+      )
     console.log(nome)
     console.log(dataNasc)
     console.log(email)
